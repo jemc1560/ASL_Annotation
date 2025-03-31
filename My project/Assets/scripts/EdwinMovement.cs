2cd524196e9b79dc0579942d682751a8ca12e175
jade <msfemjem@gmail.com>
2025-03-31 17:15:58 -0400

    annotation page ui
    progress

--- My project/Assets/scripts/EdwinMovement.cs
+++ My project/Assets/scripts/EdwinMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class EdwinMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 3f;
     [SerializeField] public FixedJoystick dynamicJoystick;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
