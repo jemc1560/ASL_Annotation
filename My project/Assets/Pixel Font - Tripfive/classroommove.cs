3aad0081e0ba69c1007e71ac35310515cb225938
Tarun <travikumar7@gatech.edu>
2025-04-07 01:23:47 -0400

    Added colliders to move to classroom + other changes

---
+++ My project/Assets/Pixel Font - Tripfive/classroommove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClassroomMove : MonoBehaviour
+{
+    // The build index of the scene to load.
+    public int sceneBuildIndex;
+
+    // Called when another collider enters this GameObject's trigger collider.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Entered");
+
+        // Check if the collider belongs to the player.
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
+
